caab909a3e61696f90e243c18c870e56c3bd2869
Michael Booth <61957561+michaelrbooth@users.noreply.github.com>
2025-08-11 13:47:12 -0700

    Update RavenSoulApplication.kt

--- src/main/kotlin/com/bricknbyte/ravenSoul/RavenSoulApplication.kt
+++ src/main/kotlin/com/bricknbyte/ravenSoul/RavenSoulApplication.kt
@@ -9,20 +9,3 @@ class RavenSoulApplication
 fun main(args: Array<String>) {
     runApplication<RavenSoulApplication>(*args)
 }
-/**
-gcloud beta run domain-mappings create --service ravensoulwellness --domain ravensoulwellness.com --region us-west1
-
-gcloud run domain-mappings delete --domain ravensoulwellness.com --region us-west2
-
-gcloud run services delete ravensoulwellness --region us-west2
-
-gcloud run deploy comsite --source . --platform managed --region us-west1 --timeout 300 --memory 512Mi --allow-unauthenticated
-
-
-gcloud run domain-mappings create --service comsite --domain ravensoulwellness.com --region us-west1
-gcloud projects add-iam-policy-binding comsite --member="user:michael@ravensoulwellness.net" --role="roles/storage.admin"
-
-gcloud projects add-iam-policy-binding comsite --member="user:michael@ravensoulwellness.net" --role="roles/run.developer"
-gcloud projects add-iam-policy-binding comsite --member="user:michael@ravensoulwellness.net" --role="roles/iam.serviceAccountUser"
-
- **/
